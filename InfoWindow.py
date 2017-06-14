27f0f306620356b3453529be6f028998a2fec7c6
Karol Majka <karol.x.majka@gsk.com>
2017-06-14 12:50:17 +0200

    dodanie kilku pól do wyświetlania informacji (raczej nie będzie potrzebne w ostateczności)

--- InfoWindow.py
+++ InfoWindow.py
@@ -2,7 +2,7 @@
 
 import sys, os, random
 from PyQt5.QtWidgets import QMainWindow, QAction, QApplication,\
-    QTextEdit, QFileDialog, QSizePolicy, QVBoxLayout, QWidget, QMdiSubWindow, QScrollArea
+    QTextEdit, QFileDialog, QSizePolicy, QVBoxLayout, QWidget, QMdiSubWindow, QScrollArea, QTableWidget, QTableWidgetItem
 from PyQt5.QtGui import QIcon
 from PyQt5 import QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -16,8 +16,17 @@ class InfoWindow(QMainWindow):
     mtbGui = None
     def __init__(self, parent=None, mtbGui=None):
         super(InfoWindow, self).__init__(parent)
-        mtbGui=self.mtbGui
+        self.mtbGui = mtbGui
         self.createWindow()
 
     def createWindow(self):
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(4)
+        self.tableWidget.setColumnCount(3)
+
+        self.tableWidget.setItem(0, 0, QTableWidgetItem("nacisk"))
+        self.tableWidget.setItem(0, 1, QTableWidgetItem("predkosc"))
+        self.tableWidget.setItem(0, 2, QTableWidgetItem("inne parametry"))
+
+        self.tableWidget.setParent(self)
         pass
